ab24e9995197c275105b4359b241c9bc6b5b0a3c
NABE5058118 <galimovkamil613@gmail.com>
2026-08-31 19:55:03 +0300

    испраление бесконечной перезагрузки фронта

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 set -e
 if [ -d "/app/dist-fresh" ]; then
-  rm -rf /app/dist
-  mkdir -p /app/dist
+  find /app/dist -mindepth 1 -delete 2>/dev/null || true
   cp -r /app/dist-fresh/. /app/dist/
 fi
 exec "$@"
