46e763987243338bfa49a6e53d0a85357d0b789e
KyKyRuZa <ituhvatullin8@gmail.com>
2026-09-08 19:47:44 +0300

    Update Navbar.tsx

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -45,7 +45,6 @@ export default function Navbar() {
         <div className={styles.links}>
           <Link to="/pricing">Тарифы</Link>
           <Link to="/dashboard/instructions">Инструкции</Link>
-          <Link to="/admin">Admin</Link>
         </div>
 
         <div className={styles.actions}>
